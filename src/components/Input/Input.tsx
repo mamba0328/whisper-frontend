0202e26f8e621a76843c1aac9df1c3cc1153a6eb
valentindj <valentindj@starway-media.com>
2024-03-11 13:30:35 +0200

    Input functionality extended

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -5,21 +5,36 @@ type InputTags = {
     id: string
     placeholder?: string,
     type: string,
+    autocomplete?: "off",
 }
 
 type Props = {
+    inputClassName?: string,
     legend: string,
     inputTags: InputTags,
     wrapperClassName?: string,
     children?: React.ReactNode,
+    value?: string,
+    onChange?: CallableFunction,
+    onValid?: CallableFunction,
 }
 
-export const Input = ({ legend, inputTags, wrapperClassName, children }:Props) => {
+export const Input = ({ value, legend, inputTags, inputClassName, wrapperClassName, children, onChange, onValid }:Props) => {
+    const handleChange = (event:React.ChangeEvent<HTMLInputElement>):void => {
+        if (!onChange) return;
+
+        if (onValid && onValid(event.target.value)) {
+            onChange(event);
+        }
+    };
+
+
     return (
-        <label className={wrapperClassName}>
-            <fieldset className={"w-full p-[7px] pt-[2px] rounded-md border border-input-search-border-color hover:border-primary-color [&>legend]:hover:text-primary-color [&:has(input:focus)]:border-2 [&:has(input:focus)>legend]:font-medium relative"}>
+        <label>
+            <fieldset className={`w-full p-[7px] pt-[2px] rounded-md border border-input-search-border-color hover:border-primary-color [&>legend]:hover:text-primary-color [&:has(input:focus)]:border-2  [&:has(input:focus)]:border-primary-color [&:has(input:focus)>legend]:font-medium relative 
+            ${wrapperClassName}`}>
                 <legend className={"ml-[5px] text-xs text-secondary-text-color "}>{legend}</legend>
-                <input {...inputTags} className={"focus:outline-none bg-transparent w-full h-full py-[5px] px-[10px] text-primary-text-color placeholder-secondary-color pr-[30px]"}/>
+                <input value={value} {...inputTags} className={`focus:outline-none bg-transparent w-full h-full py-[5px] px-[10px] text-primary-text-color placeholder-secondary-color caret-primary-text-color ${inputClassName} `} onChange={(event) => handleChange(event)}/>
                 {children}
             </fieldset>
         </label>
