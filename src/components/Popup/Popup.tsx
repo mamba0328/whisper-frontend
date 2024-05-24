f606dfb7a2cfc55be0ac8b3338fe8c2131c2a167
valentindj <valentindj@starway-media.com>
2024-05-24 10:52:34 +0300

    Popup prevent default behavior on right-click

--- src/components/Popup/Popup.tsx
+++ src/components/Popup/Popup.tsx
@@ -9,12 +9,8 @@ type Props = {
     }
 }
 export const Popup = ({ children, onClose, position }:Props) => {
-    // const propsPositionStyles = `top-[${position?.top}px] left-[${position?.left}px]`;
-    // const defaultPositionStyles = "top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2";
-    // const positionStyles = position ? propsPositionStyles : defaultPositionStyles;
-
     return (
-        <div onMouseLeave={onClose} style={position} className={"grid place-content-center fixed p-[50px] z-10 -translate-x-[50px] -translate-y-[50px] [&_li]:menu-item"}>
+        <div onMouseLeave={onClose} onContextMenu={(e) => e.preventDefault()} style={position} className={"grid place-content-center fixed p-[50px] z-20 -translate-x-[50px] -translate-y-[50px] [&_li]:menu-item"}>
             <div className={"p-[5px] rounded-lg bg-surface-color opacity-95"}>
                 {children}
             </div>
