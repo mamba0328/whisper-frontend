a51efb23ba9c72e5c85a29e91f097d4e98327403
valentindj <valentindj@starway-media.com>
2024-12-16 16:20:58 +0200

    newMessageNotification socket event handler init

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { RouterProvider } from "react-router-dom";
 
 import { CurrentUserIdContextProvider } from "./context/CurrentUserIdContext/CurrentUserIdContext";
@@ -6,7 +6,24 @@ import { CurrentUserIdContextProvider } from "./context/CurrentUserIdContext/Cur
 import { router } from "./router";
 
 import "./style/index.css";
+import { Message } from "./types/types";
+import { socket } from "./services/SocketService/SocketService";
+import { useGlobalStore } from "./store/store";
 const App = () => {
+    const { updateChatsPreviewMessage } = useGlobalStore();
+
+    useEffect(() => {
+        function handleNewMessage (newMessage:Message) {
+            updateChatsPreviewMessage(newMessage);
+        }
+
+        socket.on("newMessageNotification", handleNewMessage);
+
+        return () => {
+            socket.off("newMessageNotification", handleNewMessage);
+        };
+    }, []);
+
     return (
         <main className={"font-regular"}>
             <CurrentUserIdContextProvider>
